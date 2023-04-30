b78d27555658ef968c3d98dceaaf21807b2186f3
karl <karlbayron18@gmail.com>
2023-04-30 17:45:20 +0800

    added css for homepage and updated HomePage

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,10 +1,33 @@
 
 import React from 'react';
+import HeaderSection from '../components/HeaderSection';
+import ContentSection from '../components/ContentSection';
+import FooterSection from '../components/FooterSection';
+import Card from '../components/Card';
+import image from '../assets/self_photo.jpg'
 
 export default function HomePage() {
     return (
-        <div>
-            <p>Hello I am Karl!</p>
-        </div>
+        <>
+            <HeaderSection>
+                <div className='intro-container'>
+                    <div className='img-self-container'>
+                        <img className='img-self' src={ image } alt='self'/>
+                    </div>
+                    <div className='intro-text-container'>
+                        <h1 className='intro-title'> Hi I&apos;m Karl!</h1>
+                        <p className='intro-body'>As a software engineer, I am passionate about writing clean, readable, and testable code. With extensive experience in NodeJS, Typescript, React, Redux, and AWS Services, I am well-equipped to tackle even the most complex software development projects.</p>
+                    </div>
+                </div>
+            </HeaderSection>
+            <ContentSection>
+                <Card>
+                    <div className='intro-text-container'>
+                        <p>Hi I&apos;m Karl</p>
+                    </div>
+                </Card>
+            </ContentSection>
+            <FooterSection/>
+        </>
     )
 }
